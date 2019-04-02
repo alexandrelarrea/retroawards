bfaecca4b0836b2497632333ee535f3c94923b00
Alexandre Larrea <a.larrea@visit.brussels>
2019-04-02 15:54:50 +0200

    [Achievement Card] Update design

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,6 @@ class HomeController extends AbstractController {
    * @Route("/", name="home")
    */
   public function index() {
-    return $this->render('Home/index.html.twig');
+    return $this->render('home/index.html.twig');
   }
 }
