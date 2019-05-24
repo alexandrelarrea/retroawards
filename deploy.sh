b9938a63bf4f311b1953437d2177f785782169a5
Alexandre Larrea <a.larrea@visit.brussels>
2019-05-24 14:21:32 +0200

    [Deploy] Add package managers hook checks

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
+runOnChange() {
+    echo "$(git diff --name-only ORIG_HEAD)" | grep -q "$1" && eval "$2"
+}
+
 if [ $# -eq 0 ]
 then
     echo "### Running DEPLOY run ###"
     git stash save -u
     git pull --ff-only
     php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration --all-or-nothing
-    composer install --no-dev --optimize-autoloader
-    npm install --production
-    npm run build
+    runOnChange composer.lock "composer install --no-dev --optimize-autoloader"
+    runOnChange package-lock.json "npm install --production"
+    runOnChange assets/ "npm run build"
     php bin/console cache:clear
     php bin/console cache:warmup
 elif [ $1 == "-n" ] || [ $1 == "--dry-run" ]
