d38d2907fac53c429cbaefd792500822c1387618
Alexandre Larrea <a.larrea@visit.brussels>
2019-05-24 14:24:29 +0200

    [Deploy] Set dry run quiet mode

--- deploy.sh
+++ deploy.sh
@@ -18,16 +18,14 @@ then
 elif [ $1 == "-n" ] || [ $1 == "--dry-run" ]
 then
     echo "### Running DRY run ###"
-    git fetch
-    git status
+    git fetch -q
     git stash save -u
     git log --oneline --decorate ..origin/master
     git diff --name-status ..origin/master
     git checkout origin/master -- src/Migrations
     php bin/console doctrine:migrations:status
     php bin/console doctrine:migrations:up-to-date
-    git reset --hard
-    git stash list
+    git reset --hard -q
 else
     echo "Error : Invalid arguments"
 fi
